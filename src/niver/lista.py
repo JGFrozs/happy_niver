c9eb550f316ec1b8d90593ec8691b5f53aa33cda
Gabriel <jgfrozs@gmail.com>
2026-05-11 23:48:42 -0300

    Fix: Atualizando lista e dependências

--- src/niver/lista.py
+++ src/niver/lista.py
@@ -3,7 +3,10 @@ aniversariantes = {
     "Paula":"yyyy-04-29",
     "Alice": "yyyy-05-12",
     "Davi": "yyyy-05-12",
+    "Netinho":"yyyy-05-30",
+    "Pricila":"yyyy-05-23",
     "Maria da Graça":"yyyy-08-18",
     "Abraão":"yyyy-10-14",
     "Gloria":"yyyy-11-07",
+    "kerlinda":"yyyy-12-07"
 }
