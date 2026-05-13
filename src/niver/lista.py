ce1e7405f87af29a46e8056db79649dfc5191656
JGFROZS <148399034+JGFrozs@users.noreply.github.com>
2026-05-13 15:03:09 -0300

    Update lista.py

--- src/niver/lista.py
+++ src/niver/lista.py
@@ -4,7 +4,7 @@ aniversariantes = {
     "Alice": "yyyy-05-12",
     "Davi": "yyyy-05-12",
     "Netinho":"yyyy-05-30",
-    "Pricila":"yyyy-05-23",
+    "Priscila":"yyyy-05-23",
     "Maria da Graça":"yyyy-08-18",
     "Abraão":"yyyy-10-14",
     "Gloria":"yyyy-11-07",
